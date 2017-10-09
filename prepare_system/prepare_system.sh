df14823aefbfa85a782549a69170c54619ec720d
mi3z <github@q00c.com>
2017-10-09 12:02:37 +0200

    Update prepare_system.sh

--- prepare_system/prepare_system.sh
+++ prepare_system/prepare_system.sh
@@ -21,7 +21,7 @@ apt-get -y install vim screen terminator curl rake git
 apt-get -y install open-vm-tools-desktop
 
 ## remove shopping lenses from ubuntu
-sudo apt-get remove unity-webapps-common
+sudo apt-get remove -y unity-webapps-common
 
 ## replace german folders with english folders
 ls -d /home/*/* | grep -q "Desktop"
