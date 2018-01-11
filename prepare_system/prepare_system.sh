dfa219747f16e8091d8c023aa90d7809debf7a1a
mi3z <github@q00c.com>
2018-01-11 11:08:56 +0100

    Update prepare_system.sh
    fail2ban

--- prepare_system/prepare_system.sh
+++ prepare_system/prepare_system.sh
@@ -17,7 +17,7 @@ fi
 
 ## update and install packages
 apt-get update
-apt-get -y install vim screen terminator curl rake git
+apt-get -y install vim screen terminator curl rake git fail2ban 
 apt-get -y install open-vm-tools-desktop
 
 ## remove shopping lenses from ubuntu
